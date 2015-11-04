dee52efc5b9abe766d16fc91114f8479d7136f16
Leon <Leon@Leon-PC>
2015-11-04 18:23:34 +0100

    5. Experimental Encryption

--- CloudServer/src/ClientThread.java
+++ CloudServer/src/ClientThread.java
@@ -1,16 +1,25 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.SecureRandom;
 
-import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 public class ClientThread extends Thread{
 	
@@ -18,8 +27,12 @@ public class ClientThread extends Thread{
 	private DataInputStream dis = null;
 	private DataOutputStream dos = null;
 	private ObjectOutputStream oos = null;
+	private ObjectInputStream ois = null;
+	private InputStream is;
+	private OutputStream os;
 	private PrivateKey priv_key;
 	private PublicKey pub_key;
+	private SecretKey aes_key;
 	
 	public ClientThread(Socket socket){
 		this.socket = socket;
@@ -39,9 +52,10 @@ public class ClientThread extends Thread{
 		}
 		
 		try {
-			dis = new DataInputStream(socket.getInputStream());
-			dos = new DataOutputStream(socket.getOutputStream());
-			oos = new ObjectOutputStream(dos);
+			dis = new DataInputStream(is = socket.getInputStream());
+			dos = new DataOutputStream(os = socket.getOutputStream());
+			oos = new ObjectOutputStream(os);
+			ois = new ObjectInputStream(is);
 		} catch (IOException e) {
 			e.printStackTrace();
 			oos = null;
@@ -100,6 +114,45 @@ public class ClientThread extends Thread{
 			oos.writeObject(pub_key);
 			oos.flush();
 			System.out.println(ipString() + "> Key transmitted");
+			System.out.println("Applying key to connection...");
+			try {
+				Cipher c = Cipher.getInstance("RSA");
+				c.init(Cipher.DECRYPT_MODE, priv_key);
+				setInputCipher(c);
+			} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			sendCommand(Command.OK);
+			break;
+			
+		case AES_TRANSMISSION:
+			if(Command.get(dis.readInt()) != Command.OBJECT_TRANSMISSION){
+				System.out.println("Client does not transmit the key");
+				break;
+			}
+			try {
+				System.out.println("Waiting for AESKey");
+				ois = new ObjectInputStream(dis);
+				aes_key = (SecretKey) ois.readObject();
+				sendCommand(Command.OK);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Error while key transmission");
+				break;
+			}
+			try {
+				Cipher cin = Cipher.getInstance("AES/CTR/NoPadding");
+				Cipher cout = Cipher.getInstance("AES/CTR/NoPadding");
+				cin.init(Cipher.DECRYPT_MODE, aes_key);
+				cout.init(Cipher.ENCRYPT_MODE, aes_key);
+				setInputCipher(cin);
+				setOutputCipher(cout);
+			} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			break;
 			
 		default:
@@ -107,10 +160,43 @@ public class ClientThread extends Thread{
 			break;
 		}
 	}
+	public void setOutputCipher(Cipher c){
+		try {
+			dos = null;
+			oos = null;
+			System.out.println("Create CipherOutputStream");
+			CipherOutputStream cos = new CipherOutputStream(os = socket.getOutputStream(), c);
+			System.out.println("Create DataOutputStream");
+			dos = new DataOutputStream(cos);
+			System.out.println("Create ObjectOutputStream");
+			oos = new ObjectOutputStream(dos);
+			System.out.println("----------Done!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void setInputCipher(Cipher c){
+		try {
+			dis = null;
+			ois = null;
+			System.out.println("Create CipherInputStream");
+			CipherInputStream cis = new CipherInputStream(is = socket.getInputStream(), c);
+			System.out.println("Create DataInputStream");
+			dis = new DataInputStream(cis);
+			System.out.println("----------Done!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	public void close() throws IOException{
 		dis.close();
 		dos.close();
+		oos.close();
+		ois.close();
 		socket.close();
 		TransmissionServer.removeClient(this);
 	}
